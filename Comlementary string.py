3207ceecfc2cbeccd7370b755703f0717c2ac9b2
MichaelaGavri <mishel.gavrilovic@gmail.com>
2026-01-28 13:32:27 +0100

    changed some things

--- Comlementary string.py
+++ Comlementary string.py
@@ -1,4 +1,5 @@
-'''In DNA strings, symbols 'A' and 'T' are complements of each other, as are 'C' and 'G'.
+'''
+In DNA strings, symbols 'A' and 'T' are complements of each other, as are 'C' and 'G'.
 
 The reverse complement of a DNA string s is the string sc formed by reversing the symbols of s,
 then taking the complement of each symbol (e.g., the reverse complement of "GTCA" is "TGAC").
@@ -6,18 +7,6 @@ then taking the complement of each symbol (e.g., the reverse complement of "GTCA
 Given: A DNA string s of length at most 1000 bp.
 
 Return: The reverse complement sc of s.
-
-Pseudocode:
-
-input S
-reverse S
-for i = 1 to length of S
-     if S[i] = 'A' then S'[i] = 'T'
-     if S[i] = 'T' then S'[i] = 'A'
-     if S[i] = 'C' then S'[i] = 'G'
-     if S[i] = 'G' then S'[i] = 'C'
-
-print S'
 '''
 
 
